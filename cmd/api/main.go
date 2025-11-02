1a72228e0eac5fd8236704bcc10e49faae025d15
Jorge Eduardo <jorge.santista10@hotmail.com>
2025-11-01 21:37:55 -0300

    rotas http com chi (usando net/http do go)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 )
+
+type Carro struct {
+	Name string
+	Age  int
+}
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
+	r.Get("/{username}", func(w http.ResponseWriter, r *http.Request) {
+		param := chi.URLParam(r, "Jorge")
+		w.Write([]byte(param))
+	})
+
+	r.Post("/create_email", func(w http.ResponseWriter, r *http.Request) {
+
+		var car Carro
+		render.DecodeJSON(r.Body, &car)
+		fmt.Println(car)
+
+		render.JSON(w, r, car)
 	})
 	http.ListenAndServe(":3000", r)
 
--- go.mod
+++ go.mod
@@ -3,13 +3,15 @@ module email_campaign
 go 1.25.3
 
 require (
+	github.com/go-chi/chi/v5 v5.2.3
 	github.com/rs/xid v1.3.0
 	github.com/stretchr/testify v1.11.1
 )
 
 require (
+	github.com/ajg/form v1.5.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/go-chi/render v1.0.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
